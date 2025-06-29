3dbc0ff5b6743925e23d3689ba853c07b2135568
biqu <biqu@sapphire-c235f85>
2025-06-29 13:20:51 +0000

    adxl_x.sh modified - 06/29/2025 - 01:20:48 PM

--- printer_data/config/macros/adxl_x.sh
+++ printer_data/config/macros/adxl_x.sh
@@ -1,1 +1,1 @@
-~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_x_*.csv -o ~/printer_data/config/macros/shaper_calibrate_x.png
+~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_x_*.csv -o ~/printer_data/config/macros/shell_command/shaper_calibrate_x.png
